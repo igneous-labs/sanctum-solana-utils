d3be354761fe5336963c684adfcd615f127080a1
billythedummy <dhy1996@live.com.sg>
2023-12-14 01:55:19 +0800

    ATA

---
+++ libs/sanctum-associated-token-lib/src/instructions.rs
@@ -0,0 +1,95 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint::ProgramResult,
+    instruction::Instruction,
+    program::{invoke, invoke_signed},
+    pubkey::Pubkey,
+};
+use spl_associated_token_account::instruction::create_associated_token_account;
+
+pub const CREATE_ATA_ACCOUNTS_LEN: usize = 6;
+
+#[derive(Clone, Copy, Debug)]
+pub struct CreateAtaAccounts<'me, 'info> {
+    pub payer: &'me AccountInfo<'info>,
+    pub ata_to_create: &'me AccountInfo<'info>,
+    pub wallet: &'me AccountInfo<'info>,
+    pub mint: &'me AccountInfo<'info>,
+    pub system_program: &'me AccountInfo<'info>,
+    pub token_program: &'me AccountInfo<'info>,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct CreateAtaKeys {
+    pub payer: Pubkey,
+    pub ata_to_create: Pubkey,
+    pub wallet: Pubkey,
+    pub mint: Pubkey,
+    pub system_program: Pubkey,
+    pub token_program: Pubkey,
+}
+
+impl CreateAtaKeys {
+    pub fn to_ix(&self) -> Instruction {
+        create_associated_token_account(&self.payer, &self.wallet, &self.mint, &self.token_program)
+    }
+}
+
+impl From<CreateAtaAccounts<'_, '_>> for CreateAtaKeys {
+    fn from(
+        CreateAtaAccounts {
+            payer,
+            ata_to_create,
+            wallet,
+            mint,
+            system_program,
+            token_program,
+        }: CreateAtaAccounts<'_, '_>,
+    ) -> Self {
+        Self {
+            payer: *payer.key,
+            ata_to_create: *ata_to_create.key,
+            wallet: *wallet.key,
+            mint: *mint.key,
+            system_program: *system_program.key,
+            token_program: *token_program.key,
+        }
+    }
+}
+
+impl<'info> From<CreateAtaAccounts<'_, 'info>> for [AccountInfo<'info>; CREATE_ATA_ACCOUNTS_LEN] {
+    fn from(
+        CreateAtaAccounts {
+            payer,
+            ata_to_create,
+            wallet,
+            mint,
+            system_program,
+            token_program,
+        }: CreateAtaAccounts<'_, 'info>,
+    ) -> Self {
+        [
+            payer.clone(),
+            ata_to_create.clone(),
+            wallet.clone(),
+            mint.clone(),
+            system_program.clone(),
+            token_program.clone(),
+        ]
+    }
+}
+
+pub fn create_ata_invoke(accounts: CreateAtaAccounts) -> ProgramResult {
+    let ix = CreateAtaKeys::from(accounts).to_ix();
+    let account_infos: [AccountInfo; CREATE_ATA_ACCOUNTS_LEN] = accounts.into();
+    invoke(&ix, &account_infos)
+}
+
+pub fn create_ata_invoke_signed(
+    accounts: CreateAtaAccounts,
+    signer_seeds: &[&[&[u8]]],
+) -> ProgramResult {
+    let ix = CreateAtaKeys::from(accounts).to_ix();
+    let account_infos: [AccountInfo; CREATE_ATA_ACCOUNTS_LEN] = accounts.into();
+    invoke_signed(&ix, &account_infos, signer_seeds)
+}
---
+++ libs/sanctum-associated-token-lib/src/lib.rs
@@ -0,0 +1,5 @@
+mod instructions;
+mod pda;
+
+pub use instructions::*;
+pub use pda::*;

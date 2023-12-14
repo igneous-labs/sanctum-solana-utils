2e05bf4d2f7e0c928c287e6ee72deb0525cf42e5
billythedummy <dhy1996@live.com.sg>
2023-12-14 19:08:51 +0800

    add paths

--- sanctum-solana-test-utils/src/lib.rs
+++ sanctum-solana-test-utils/src/lib.rs
@@ -3,6 +3,7 @@ mod extended_banks_client;
 mod extended_program_test;
 mod into_account;
 mod keyed_ui_account;
+mod paths;
 mod tx;
 
 pub use consts::*;
@@ -10,6 +11,7 @@ pub use extended_banks_client::*;
 pub use extended_program_test::*;
 pub use into_account::*;
 pub use keyed_ui_account::*;
+pub use paths::*;
 pub use tx::*;
 
 #[cfg(feature = "token")]

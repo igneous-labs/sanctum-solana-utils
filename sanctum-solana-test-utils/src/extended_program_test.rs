c897a5965577e02cda63888c737b90b3183c2c9e
billythedummy <dhy1996@live.com.sg>
2023-12-18 16:30:59 +0800

    add_test_fixtures_account

--- sanctum-solana-test-utils/src/extended_program_test.rs
+++ sanctum-solana-test-utils/src/extended_program_test.rs
@@ -5,7 +5,7 @@ use solana_program_test::ProgramTest;
 use solana_readonly_account::sdk::KeyedAccount;
 use solana_sdk::account::Account;
 
-use crate::KeyedUiAccount;
+use crate::{test_fixtures_dir, KeyedUiAccount};
 
 /// For nice method syntax on `ProgramTest`
 pub trait ExtendedProgramTest {
@@ -13,6 +13,7 @@ pub trait ExtendedProgramTest {
     fn add_keyed_account(self, keyed_account: KeyedAccount) -> Self;
     fn add_keyed_ui_account(self, keyed_ui_account: KeyedUiAccount) -> Self;
     fn add_account_from_file<P: AsRef<Path>>(self, json_file_path: P) -> Self;
+    fn add_test_fixtures_account<P: AsRef<Path>>(self, relative_json_file_path: P) -> Self;
     fn add_system_account(self, address: Pubkey, lamports: u64) -> Self;
 }
 
@@ -32,6 +33,11 @@ impl ExtendedProgramTest for ProgramTest {
 
     fn add_account_from_file<P: AsRef<Path>>(self, json_file_path: P) -> Self {
         self.add_keyed_ui_account(KeyedUiAccount::from_file(json_file_path))
+    }
+
+    /// Adds a KeyedUiAccount from `<test_fixtures_dir()>/relative_json_file_path`
+    fn add_test_fixtures_account<P: AsRef<Path>>(self, relative_json_file_path: P) -> Self {
+        self.add_account_from_file(test_fixtures_dir().join(relative_json_file_path))
     }
 
     fn add_system_account(self, address: Pubkey, lamports: u64) -> Self {

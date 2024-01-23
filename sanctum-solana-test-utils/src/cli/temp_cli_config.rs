0d4895194c3f1985649f8b3362e27a67f73b668e
billythedummy <dhy1996@live.com.sg>
2024-01-24 01:13:33 +0800

    more general

--- sanctum-solana-test-utils/src/cli/temp_cli_config.rs
+++ sanctum-solana-test-utils/src/cli/temp_cli_config.rs
@@ -4,15 +4,14 @@ use solana_cli_config::Config;
 use solana_sdk::signature::Keypair;
 use tempfile::NamedTempFile;
 
-/// `NamedTempFile` is deleted when dropped
+/// Contained `NamedTempFile`s are deleted when dropped
 pub struct TempCliConfig {
     keypair: NamedTempFile,
     config: NamedTempFile,
 }
 
 impl TempCliConfig {
-    /// Config will point to `http://127.0.0.1:{port}`
-    pub fn from_port_and_keypair(keypair: &Keypair, port: u16) -> Self {
+    pub fn from_keypair_and_rpc_url(keypair: &Keypair, json_rpc_url: String) -> Self {
         let kp_bytes = keypair.to_bytes();
         let keypair = NamedTempFile::new().unwrap();
         serde_json::to_writer(keypair.as_file(), kp_bytes.as_ref()).unwrap();
@@ -21,7 +20,7 @@ impl TempCliConfig {
         serde_yaml::to_writer(
             config.as_file(),
             &Config {
-                json_rpc_url: format!("http://127.0.0.1:{port}"),
+                json_rpc_url,
                 websocket_url: "".to_owned(),
                 keypair_path: keypair.path().to_str().unwrap().to_owned(),
                 address_labels: HashMap::new(),
@@ -32,8 +31,13 @@ impl TempCliConfig {
         Self { keypair, config }
     }
 
+    /// Config will point to `http://127.0.0.1:{port}`
+    pub fn from_keypair_and_local_port(keypair: &Keypair, port: u16) -> Self {
+        Self::from_keypair_and_rpc_url(keypair, format!("http://127.0.0.1:{port}"))
+    }
+
     // Access keypair and config via getters to ensure this struct is never
-    // destructured, since destructuring can drop unused fields = delete file
+    // destructured, since destructuring can drop unused fields => delete file
 
     pub fn keypair(&self) -> &NamedTempFile {
         &self.keypair
@@ -55,7 +59,7 @@ mod tests {
     fn temp_cli_config_basic() {
         let port = 12321;
         let kp = Keypair::new();
-        let tcc = TempCliConfig::from_port_and_keypair(&kp, 12321);
+        let tcc = TempCliConfig::from_keypair_and_local_port(&kp, 12321);
         let config = ConfigWrapper::parse_from_path(tcc.config().path().to_str().unwrap()).unwrap();
         assert_eq!(
             config.as_ref().json_rpc_url,
